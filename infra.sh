53b54c5ed6d177f462c83a7c4423b8e41caedb54
Kiran <kiranops@yahoo.com>
2021-10-06 07:16:28 +0530

    added steps to create VNET

--- infra.sh
+++ infra.sh
@@ -5,3 +5,6 @@ source ./vars.txt
 
 echo "Creating Resource Group :$RESOURCE_GROUP"
 az group create --name $RESOURCE_GROUP --location $REGION
+
+
+az network create --name $VNET_NAME --cidr-block $VNET_CIDR 
