452ead5ae5c8f74e395b47377bea9241137b3085
KiKiran <kirankumarops@gmail.com>
2021-10-06 02:48:49 +0000

    added vm creation

--- infra.sh
+++ infra.sh
@@ -10,3 +10,6 @@ az group create --name $RESOURCE_GROUP --location $REGION
 az network vnet create --name $VNET_NAME \
                        --resource-group $RESOURCE_GROUP 
                        --cidr-block $VNET_CIDR
+
+
+az vm create --name $VM_NAME --resource-group $RESOURCE_GROUP --location eastus
